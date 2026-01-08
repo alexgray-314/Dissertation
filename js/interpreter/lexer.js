0e1002351c8ef1c2bf29571f12b735441240b4b3
Alex Gray <alexjgray@live.co.uk>
2026-01-08 11:38:10 +0000

    Added new features to the lexer

--- js/interpreter/lexer.js
+++ js/interpreter/lexer.js
@@ -24,6 +24,60 @@ class Lexer {
     this.input = s;
   }
 
+  #getDigit() {
+    let lexeme = "";
+    while(/\d/.test(this.#peek())) {
+      const c = this.#peek();
+      this.#eat(c);
+      lexeme += c;
+    }
+    return Number(lexeme);
+  }
+
+  #lex_number() {
+    const digit = this.#getDigit();
+    return {
+      token:"NUMBER",
+      value: digit
+    }
+  }
+
+  #lex_player() {
+
+    switch(this.#peek()) {
+      case '#':
+        this.#eat('#');
+        return {
+          token:"PLAYER",
+          value:"MOVE"
+        };
+      case '/':
+        this.#eat('/');
+        return {
+          token:"PLAYER",
+          value:"ACTION"
+        };
+      case '.':
+        this.#eat('.');
+        return {
+          token:"PLAYER",
+          value:"TURN",
+        };
+      default:
+        if (/\d/.test(this.#peek())) {
+          const digit = this.#getDigit();
+          return {
+            token:"PLAYER",
+            value:digit
+          };
+        } else {
+          throw "invalid character";
+        }
+    }
+
+
+  }
+
   #lex_kw_or_id() {
     let lexeme = "";
     while(/[A-Za-z]/.test(this.#peek())) {
@@ -58,8 +112,35 @@ class Lexer {
       } else if (/[A-Za-z]/.test(c)) {
         tokens.push(this.#lex_kw_or_id());
       } else if (/\s/.test(c)) {
+        // whitespace
         this.#eat(c);
+      } else if (this.#peek() === '<') {
+        this.#eat('<');
+        if (this.#peek() === '<') {
+          this.#eat('<')
+          tokens.push({
+            token:"LESS_THAN"
+          });
+        } else {
+          tokens.push(this.#lex_player());
+          this.#eat('>');
+        }
+      } else if (/\d/.test(this.#peek())) {
+        tokens.push(this.#lex_number());
+      } else if (this.#peek() === '.') {
+        this.#eat('.');
+        if (this.#peek() === '.') {
+          this.#eat('.');
+          tokens.push({
+            token:"DOUBLE_DOT"
+          })
+        } else {
+          tokens.push({
+            token:"DOT"
+          })
+        }
       }
+
 
     }
     tokens.push({
--- js/interpreter/token.js
+++ js/interpreter/token.js
@@ -1,14 +1,21 @@
 const keyWords = {
   "define": "DEFINE",
   "area": "AREA",
-  "event": "EVENT",
+  "action": "ACTION",
   "deck": "DECK",
+  "on": "ON",
+  "if": "IF",
+  "else": "ELSE",
 };
 
 const specialCharacters = {
-  "(": "L_PAREN",
-  ")": "R_PAREN",
-  ",": "COMMA",
-  ":": "COLON",
-  ";": "SEMI_COLON",
+  '(': "L_PAREN",
+  ')': "R_PAREN",
+  ',': "COMMA",
+  ':': "COLON",
+  ';': "SEMI_COLON",
+  '[': "L_SQUARE",
+  ']': "R_SQUARE",
+  '{': "L_CURLY",
+  '}': "R_CURLY",
 };
