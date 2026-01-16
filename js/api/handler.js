7ddbf876a9845f1e5a70eaaf21b0c925a23e5aa3
Alex Gray <alexjgray@live.co.uk>
2026-01-16 21:15:04 +0000

    Default args work properly

--- js/api/handler.js
+++ js/api/handler.js
@@ -5,14 +5,14 @@ class Handler {
 
   define_area(data) {
     const area = new Area(data.id);
-    const defaultArgs = {
+    const args = {
       id: "default",
       args: {
         "min": 3,
         "max": -1
       }
     }
-    const args = Object.assign(defaultArgs, data)// merge defaults with set parameters
+    this.#deepReplace(args, data)// merge defaults with set parameters
 
     // Setting the args
     // min
@@ -22,6 +22,20 @@ class Handler {
     }
 
     this.areas.push(area);
+  }
+
+  #deepReplace(target, source) {
+    for (const key in source) {
+      if (
+        typeof source[key] === "object" &&
+        source[key] !== null &&
+        typeof target[key] === "object"
+      ) {
+        this.#deepReplace(target[key], source[key]);
+      } else {
+        target[key] = source[key];
+      }
+    }
   }
 
 }
