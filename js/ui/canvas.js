a7ae30a987f13c55b77fa95b3781dc79f9be9c6d
Alex <xd23197@bristol.ac.uk>
2026-01-24 15:26:28 +0000

    Dragging on canvas displays movement

--- js/ui/canvas.js
+++ js/ui/canvas.js
@@ -3,18 +3,19 @@ class Canvas {
   constructor(initialState) {
     this.canvas = document.getElementById("canvas");
 
-    // Convert all the state "areas" into UI "Areas"
-    let y = 0;
-    this.areas = Object.fromEntries(
-      Object.entries(initialState.areas).map(([id, area]) => {
-        y++;
-        return [id, new Area(area, y-1)];
-      })
-    );
-
-    console.log(this.areas);
+    this.areas = {};
+    this.update(initialState);
 
     this.mouseHandler = new MouseHandler(this.canvas, this.areas);
+  }
+
+  update(state) {
+    // Convert all the state "areas" into UI "Areas"
+    let y = 0;
+    Object.entries(state.areas).forEach(([id, area]) => {
+      y++;
+      this.areas[id] = new Area(area, y-1);
+    })
   }
 
   render() {
@@ -28,8 +29,12 @@ class Canvas {
     if (this.mouseHandler.draggedCard) {
       this.mouseHandler.draggedCard.render(
         ctx,
-        this.mouseHandler.mouseX - CARD_WIDTH / 2,
-        this.mouseHandler.mouseY - CARD_HEIGHT / 2
+        {
+          x: this.mouseHandler.mouseX - CARD_WIDTH / 2,
+          y: this.mouseHandler.mouseY - CARD_HEIGHT / 2,
+          width: CARD_WIDTH,
+          height: CARD_HEIGHT
+        }
       );
     }
     
