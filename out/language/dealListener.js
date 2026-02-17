70022e7c3509404653f720b4196e76cd4531815c
Alex Gray <alexjgray@live.co.uk>
2026-02-17 16:41:07 +0000

    Somehow it's running

--- out/language/dealListener.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Generated from C:\Users\alexj\University\dealer/src/language/deal.g4 by ANTLR 4.9.0-SNAPSHOT
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=dealListener.js.map
